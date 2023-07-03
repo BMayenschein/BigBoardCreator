794533187711ec959747280a9a3227468fdbdc29
Bruce Mayenschein <brucedmayenschein@gmail.com>
2023-07-03 09:45:54 -0400

    addToBoard post method added

--- routes/ranked.js
+++ routes/ranked.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const rankedController = require('../controllers/ranked')
 
-router.get('/', rankedController.getPlayers) 
+router.get('/', rankedController.getPlayers) ;
+router.post('/addToBoard', rankedController.addToBoard);
 
 module.exports = router
