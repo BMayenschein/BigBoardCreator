d26a5bf8c174b3ac116026227c6cd59555061175
Bruce Mayenschein <brucedmayenschein@gmail.com>
2023-07-25 08:58:31 -0400

    removed ID as it is generated when added to DB

--- models/playerInfo.js
+++ models/playerInfo.js
@@ -1,10 +1,6 @@
-const { ObjectId } = require('mongodb')
 const mongoose = require('mongoose')
 
 const playerInfoSchema = new mongoose.Schema({
-    _id: {
-        type: ObjectId
-    },
     firstName: {
         type: String,
         required: true,
