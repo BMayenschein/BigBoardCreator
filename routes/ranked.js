ea765654b6064abbc62fdf28a0846ffbd7e3e555
Bruce Mayenschein <brucedmayenschein@gmail.com>
2023-07-25 08:59:51 -0400

    saveBoard route added

--- routes/ranked.js
+++ routes/ranked.js
@@ -4,5 +4,6 @@ const rankedController = require('../controllers/ranked')
 
 router.get('/', rankedController.getPlayers) ;
 router.post('/addToBoard', rankedController.addToBoard);
+router.post('/saveBoard', rankedController.saveBoard);
 
 module.exports = router
