6fe96139fd41d0e1a880c1818bc1baa73e0a855c
Bruce Mayenschein <brucedmayenschein@gmail.com>
2023-07-27 11:03:52 -0400

    Added getUserBoard route and fixed issue where all routes pointed to getPlayers method

--- routes/ranked.js
+++ routes/ranked.js
@@ -3,7 +3,14 @@ const router = express.Router()
 const rankedController = require('../controllers/ranked')
 
 router.get('/', rankedController.getPlayers) ;
+router.get('/getUserBoard', rankedController.getUserBoard);
+
+//Last route because it overrides everything
+router.get('/:username', rankedController.getPlayers);
+
+
 router.post('/addToBoard', rankedController.addToBoard);
 router.post('/saveBoard', rankedController.saveBoard);
+
 
 module.exports = router
