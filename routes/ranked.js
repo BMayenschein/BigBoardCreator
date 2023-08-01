b857f37db821c280cc6b2e01c8cd0d48d325e71a
Bruce Mayenschein <brucedmayenschein@gmail.com>
2023-08-01 11:34:11 -0400

    import and use ensureAuth and ensureGuest middleware

--- routes/ranked.js
+++ routes/ranked.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const rankedController = require('../controllers/ranked')
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-router.get('/', rankedController.getPlayers) ;
+router.get('/', ensureGuest, rankedController.getPlayers) ;
 router.get('/getUserBoard', rankedController.getUserBoard);
 
 //Last route because it overrides everything
-router.get('/:username', rankedController.getPlayers);
+router.get('/:userName', ensureAuth, rankedController.getPlayers);
 
 
 router.post('/addToBoard', rankedController.addToBoard);
