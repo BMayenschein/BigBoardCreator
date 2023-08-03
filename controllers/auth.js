5c695eb008bd5235f80d125f00eaba23769e7cbf
Bruce Mayenschein <brucedmayenschein@gmail.com>
2023-08-03 14:04:38 -0400

    Added validation to username and removed commented out code

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,18 +51,6 @@ exports.postLogin = async (req, res, next) => {
   }
 };
 
-// exports.logout = async (req, res) => {
-//   req.logout(() => {
-//     console.log('User has logged out.')
-//   });
-//   req.session.destroy((err) => {
-//     if (err)
-//       console.log("Error : Failed to destroy the session during logout.", err);
-//     req.user = null;
-//     res.redirect("/");
-//   });
-// };
-
 exports.logout = async (req, res, next) => {
   req.logout(function(err) {
     if (err) {return next(err);}
@@ -89,7 +77,8 @@ exports.postSignup = async (req, res, next) => {
     });
   if (req.body.password !== req.body.confirmPassword)
     validationErrors.push({ msg: "Passwords do not match" });
-
+  if (!validator.isAlphanumeric(req.body.userName))
+    validationErrors.push({ msg: "Usernames can only contain letters and numbers"})
   if (validationErrors.length) {
     req.flash("errors", validationErrors);
     return res.redirect("/auth/signup");
