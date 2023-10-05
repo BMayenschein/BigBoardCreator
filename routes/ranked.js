363416004914b1d87c43eb7a068419f422fa9218
Bruce Mayenschein <brucedmayenschein@gmail.com>
2023-10-05 10:29:14 -0400

    Added sort functionality to player cards by name, position, pts, rebs and asts

--- routes/ranked.js
+++ routes/ranked.js
@@ -5,6 +5,12 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 router.get('/', ensureGuest, rankedController.getPlayers) ;
 router.get('/getUserBoard', rankedController.getUserBoard);
+router.get('/sortByName', rankedController.sortByName);
+router.get('/sortByPosition', rankedController.sortByPosition);
+router.get('/sortByPoints', rankedController.sortByPoints);
+router.get('/sortByRebounds', rankedController.sortByRebounds);
+router.get('/sortByAst', rankedController.sortByAst);
+
 
 //Last route because it overrides everything
 router.get('/:userName', ensureAuth, rankedController.getPlayers);
